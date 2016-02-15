98ae7665a5e554c69e5b6c3a3d87a964c215e95e
Sam <contactsamie@gmail.com>
2016-02-14 23:11:06 -0500

    update 820

--- lightservice-timemachine.js
+++ lightservice-timemachine.js
@@ -1,66 +1,80 @@
+/**
+ * lightservice - Simple and convinient interface for service consumption
+ * @version v8.2.0
+ * @link https://github.com/contactsamie/LightService
+ * @license MIT
+ * @license Samuel Bamgboye <contactsamie@gmail.com> 
+ */
 var light = ((typeof require !== "undefined") && require('lightservice')) || light;
-light.service("timemachine_next", function (arg) {
-    var records = this.service.timemachine_record().result();
-    var recordLength = records.length;
-    var pointer = this.service.timemachine_pointer().result();
-    pointer = pointer - 2;
-    this.service.timemachine_pointer(pointer);
 
-    pointer === -1 ? this.current() : light.advanced.play(records, recordLength - (1 + pointer), recordLength - pointer);
-});
-light.service("timemachine_previous", function (arg) {
-    var records = this.service.timemachine_record().result();
-    var recordLength = records.length;
-    var pointer = this.service.timemachine_pointer().result();
-    pointer = pointer + 2;
-    this.service.timemachine_pointer(pointer);
+    light.service("timemachine_next", function (arg) {
+        var records = this.service.timemachine_record().result();
+        var pointer = this.service.timemachine_pointer().result();
+        pointer = pointer - 2;
+        this.service.timemachine_pointer(pointer).result();
 
-    pointer >= recordLength ? this.current() : light.advanced.play(records, recordLength - (1 + pointer), recordLength - pointer);
-});
-light.service("timemachine_current", function (arg) {
-    var records = this.service.timemachine_record().result();
-    var recordLength = records.length;
-    this.service.timemachine_pointer(-1);
-
-    light.advanced.play(records, recordLength - 2, recordLength - 1);
-});
-
-light.service("timemachine_play", function (arg) {
-    var records = this.service.timemachine_record().result();
-    light.advanced.play(records, arg.i, arg.j)
-});
-
-light.service("timemachine_getLastRecord", function (p) {
-    var records = this.service.timemachine_record().result();
-    return records.length ? records[records.length - 1] : [];
-});
-
-light.service("timemachine_getRecord", function (i) {
-    return this.service.timemachine_record().result()[i];
-});
-
-light.ServiceDataObject("timemachine_pointer",-1);
-
-
-light.ServiceDataList("timemachine_record");
-
-light.onSystemRecordEvent(function (e) {
-    light(function () {
-      
-        this.service.timemachine_record(JSON.parse(e));
-      
+        pointer === -1 ? this.service.timemachine_last().result() : this.service.timemachine_point(pointer).result();
     });
-});
+    light.service("timemachine_previous", function (arg) {
+        var records = this.service.timemachine_record().result();
+        var recordLength = records.length;
+        var pointer = this.service.timemachine_pointer().result();
+        pointer = pointer + 2;
+        this.service.timemachine_pointer(pointer).result();
 
+        pointer >= recordLength ? this.service.timemachine_first().result() : this.service.timemachine_point(pointer).result();
+    });
 
+    light.service("timemachine_point", function (pointer) {
+        this.service.timemachine_pointer(pointer).result();
+        var records = this.service.timemachine_record().result();
+        var recordLength = records.length;
+        light.advanced.play(records, recordLength - (1 + pointer), recordLength - pointer);
+    });
 
+    light.service("timemachine_last", function (arg) {
+        var records = this.service.timemachine_record().result();
+        var recordLength = records.length;
+        this.service.timemachine_pointer(1).result();
+        var pointer = this.service.timemachine_pointer().result();
+        light.advanced.play(records, recordLength - (1 + pointer), recordLength - pointer);
+    });
+    light.service("timemachine_first", function (arg) {
+        var records = this.service.timemachine_record().result();
+        var recordLength = records.length;
+        this.service.timemachine_pointer(recordLength - 1).result();
+        var pointer = this.service.timemachine_pointer().result();
+        light.advanced.play(records, recordLength - (1 + pointer), recordLength - pointer);
+    });
 
-var timemachine = {};
+    light.service("timemachine_play", function (arg) {
+        var records = this.service.timemachine_record().result();
+        light.advanced.play(records, arg.i, arg.j)
+    });
 
-if (typeof module !== "undefined" && ('exports' in module)) {
-    module.exports = timemachine;
-}
+    light.service("timemachine_getLastRecord", function (p) {
+        var records = this.service.timemachine_record().result();
+        return records.length ? records[records.length - 1] : [];
+    });
 
-if (typeof define === 'function' && define.amd) {
-    define('light', [], function () { return timemachine; });
-}
+    light.service("timemachine_getRecord", function (i) {
+        return this.service.timemachine_record().result()[i];
+    });
+
+    light.ServiceDataObject("timemachine_pointer", 1);
+
+    light.ServiceDataList("timemachine_record");
+
+    light.onSystemRecordEvent(function (e) {
+        light(function () {
+            this.service.timemachine_record(JSON.parse(e));
+        });
+    });
+    var timemachine={};
+    if (typeof module !== "undefined" && ('exports' in module)) {
+        module.exports = timemachine;
+    }
+
+    if (typeof define === 'function' && define.amd) {
+        define('light', [], function () { return timemachine; });
+    }
